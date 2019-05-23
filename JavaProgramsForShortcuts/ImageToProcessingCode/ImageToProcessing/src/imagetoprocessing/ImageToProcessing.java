fcc4ca3b79f5b43aa70cafb2a02ffbb54837beca
JoshuaTianYangLiu <46253730+JoshuaTianYangLiu@users.noreply.github.com>
2019-05-23 10:28:52 -0400

    Added work

--- JavaProgramsForShortcuts/ImageToProcessingCode/ImageToProcessing/src/imagetoprocessing/ImageToProcessing.java
+++ JavaProgramsForShortcuts/ImageToProcessingCode/ImageToProcessing/src/imagetoprocessing/ImageToProcessing.java
@@ -46,45 +46,91 @@ public class ImageToProcessing {
         boolean movement=readCharacter()=='Y'?true:false;
         System.out.println("Size of each pixel.");
         int pixelSize=readInt();
-        BufferedImage img = null; 
-        File f = null; 
-        FileWriter fileWriter = new FileWriter("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\out.txt");
-        FileWriter fileWriter1 = new FileWriter("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\debug.txt");
-        PrintWriter printWriter = new PrintWriter(fileWriter);
-        PrintWriter printWriter1 = new PrintWriter(fileWriter1);
-        
-        try
-        { 
-            f = new File("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\in.png"); 
-            img = ImageIO.read(f); 
-        } 
-        catch(Exception e) 
-        { 
-            System.out.println(e); 
-        } 
-        for(int i=0; i<img.getWidth(); i++){
-            for(int j=0; j<img.getHeight(); j++){
-                int p = img.getRGB(i,j);
-                int a = (p>>24) & 0xff; 
-                int r = (p>>16) & 0xff;
-                int g = (p>>8) & 0xff;
-                int b = p & 0xff; 
-                if(!ignoreColour(p)&&a!=0){
-                    //16776960
-                    System.out.printf("%d %d, %d, %d, %d\n",p,a,r,g,b);
-                    if(pixelSize<=1){
-                        if(movement)printWriter.printf("col(%d,%d,%d);\npoint(x+%d,y+%d);\n",r,g,b,i,j);
-                        else printWriter.printf("col(%d,%d,%d);\npoint(%d,%d);\n",r,g,b,i,j);
-                    }else{
-                        if(movement)printWriter.printf("col(%d,%d,%d);\rect(%d*x,%d*y,%d,%d);\n",r,g,b,pixelSize*i,pixelSize*j,pixelSize,pixelSize);
-                        else printWriter.printf("col(%d,%d,%d);\nrect(%d,%d,%d,%d);\n",r,g,b,pixelSize*i,pixelSize*j,pixelSize,pixelSize);
+        System.out.println("Number of files to convert. Each picture should be in (1), in (2), ect");
+        int times=readInt();
+        if(times==1){
+            BufferedImage img = null; 
+            File f = null; 
+            FileWriter fileWriter = new FileWriter("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\out.txt");
+            FileWriter fileWriter1 = new FileWriter("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\debug.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            PrintWriter printWriter1 = new PrintWriter(fileWriter1);
+
+            try
+            { 
+                f = new File("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\in.png"); 
+                img = ImageIO.read(f); 
+            } 
+            catch(Exception e) 
+            { 
+                System.out.println(e); 
+            } 
+            for(int i=0; i<img.getWidth(); i++){
+                for(int j=0; j<img.getHeight(); j++){
+                    int p = img.getRGB(i,j);
+                    int a = (p>>24) & 0xff; 
+                    int r = (p>>16) & 0xff;
+                    int g = (p>>8) & 0xff;
+                    int b = p & 0xff; 
+                    if(!ignoreColour(p)&&a!=0){
+                        //16776960
+                        System.out.printf("%d %d, %d, %d, %d\n",p,a,r,g,b);
+                        if(pixelSize<=1){
+                            if(movement)printWriter.printf("col(%d,%d,%d);\npoint(x+%d,y+%d);\n",r,g,b,i,j);
+                            else printWriter.printf("col(%d,%d,%d);\npoint(%d,%d);\n",r,g,b,i,j);
+                        }else{
+                            if(movement)printWriter.printf("col(%d,%d,%d);\rect(%d*x,%d*y,%d,%d);\n",r,g,b,pixelSize*i,pixelSize*j,pixelSize-1,pixelSize-1);
+                            else printWriter.printf("col(%d,%d,%d);\nrect(%d,%d,%d,%d);\n",r,g,b,pixelSize*i,pixelSize*j,pixelSize-1,pixelSize-1);
+                        }
+                        printWriter1.printf("%d %d, %d, %d, %d\n",p,a,r,g,b);
                     }
-                    printWriter1.printf("%d %d, %d, %d, %d\n",p,a,r,g,b);
                 }
             }
+            printWriter.close(); 
+            printWriter1.close(); 
+        }else{
+            for(int k=1; k<=times; k++){
+                BufferedImage img = null; 
+                File f = null; 
+                FileWriter fileWriter = new FileWriter("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\out ("+k+").txt");
+                FileWriter fileWriter1 = new FileWriter("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\debug ("+k+").txt");
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                PrintWriter printWriter1 = new PrintWriter(fileWriter1);
+
+                try
+                { 
+                    f = new File("C:\\Users\\Joshua\\Desktop\\ProcessingWorks\\JavaProgramsForShortcuts\\ImageToProcessingCode\\in ("+k+").png"); 
+                    img = ImageIO.read(f); 
+                } 
+                catch(Exception e) 
+                { 
+                    System.out.println(e); 
+                } 
+                for(int i=0; i<img.getWidth(); i++){
+                    for(int j=0; j<img.getHeight(); j++){
+                        int p = img.getRGB(i,j);
+                        int a = (p>>24) & 0xff; 
+                        int r = (p>>16) & 0xff;
+                        int g = (p>>8) & 0xff;
+                        int b = p & 0xff; 
+                        if(!ignoreColour(p)&&a!=0){
+                            //16776960
+                            System.out.printf("%d %d, %d, %d, %d\n",p,a,r,g,b);
+                            if(pixelSize<=1){
+                                if(movement)printWriter.printf("col(%d,%d,%d);\npoint(x+%d,y+%d);\n",r,g,b,i,j);
+                                else printWriter.printf("col(%d,%d,%d);\npoint(%d,%d);\n",r,g,b,i,j);
+                            }else{
+                                if(movement)printWriter.printf("col(%d,%d,%d);\rect(%d*x,%d*y,%d,%d);\n",r,g,b,pixelSize*i,pixelSize*j,pixelSize-1,pixelSize-1);
+                                else printWriter.printf("col(%d,%d,%d);\nrect(%d,%d,%d,%d);\n",r,g,b,pixelSize*i,pixelSize*j,pixelSize-1,pixelSize-1);
+                            }
+                            printWriter1.printf("%d %d, %d, %d, %d\n",p,a,r,g,b);
+                        }
+                    }
+                }
+                printWriter.close(); 
+                printWriter1.close(); 
+            }
         }
-        printWriter.close(); 
-        printWriter1.close(); 
     }
     static boolean ignoreColour(int p){
         int r = (p>>16) & 0xff;
